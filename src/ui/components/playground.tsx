99d519d2b060ca357907a5a5cd164d546a538417
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-03-22 00:13:58 +0200

    styles: change size shadow

--- src/ui/components/playground.tsx
+++ src/ui/components/playground.tsx
@@ -1,7 +1,7 @@
 export const PlayGround = () => {
   return (
     <div className="h-4/5 w-4/5 relative ">
-      <section className="h-4/5 z-50 w-3/4 mx-auto relative my-0 bg-black rounded shadow border border-zinc-900">
+      <section className="h-4/5 z-50 w-3/4 mx-auto relative my-0 bg-black rounded shadow-md border border-zinc-900">
         <header className="h-10 w-full border-b border-zinc-900 flex items-center">
           <div className="h-full gap-2 flex items-center border-r px-3 border-zinc-900">
             <span className="w-3 h-3 cursor-pointer rounded-full bg-red-600 block"></span>
