7ad423e4b362389e95dae647a48606f47bdeab83
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-03-22 00:07:40 +0200

    chore: setup tailwinds

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/app/**/*.{js,ts,jsx,tsx}', './src/ui/components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
