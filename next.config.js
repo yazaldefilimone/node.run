c71fd3bf0ea7fe8cd42711bfc948e5471aca5fef
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-03-26 23:56:14 +0200

    chore: config cors and install dependencies

--- next.config.js
+++ next.config.js
@@ -1,8 +1,28 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   experimental: {
     appDir: true,
+    esmExternals: true,
+    esmExternals: 'loose',
   },
-}
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin',
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp',
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
