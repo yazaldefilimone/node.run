6def629514d6744869d66d37801ce69081ed8578
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-03-26 23:50:08 +0200

    refactor: change add for running

--- src/ui/components/tab-file.tsx
+++ src/ui/components/tab-file.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from 'react';
 export const TabFile: FunctionComponent = () => {
   return (
     <div className="h-full gap-2 flex items-center border-r border-b border-b-black relative before:absolute before:-bottom-1 before:h-1 before:bg-black before:block before:w-full before:left-0 px-4 border-zinc-900  min-w-[80px]">
-      <span className="text-zinc-500">index.ts</span>
+      <span className="text-zinc-500">index.js</span>
       <button>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" className="fill-zinc-500">
           <path fill="none" d="M0 0h24v24H0z" />
@@ -14,14 +14,17 @@ export const TabFile: FunctionComponent = () => {
   );
 };
 
-export const TabAddFile: FunctionComponent = () => {
+type tabAddFileType = {
+  action: () => void;
+};
+
+export const TabAddFile: FunctionComponent<tabAddFileType> = ({ action }) => {
   return (
-    <div className="h-full gap-2 flex items-center border-r px-4 border-zinc-900  min-w-[80px]">
-      <span className="text-zinc-500">new file</span>
-      <button>
+    <div className="h-full gap-2 flex items-center  px-4 border-zinc-900  min-w-[80px]">
+      <button onClick={() => action()}>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" className="fill-zinc-500">
           <path fill="none" d="M0 0h24v24H0z" />
-          <path d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z" />
+          <path d="M16.394 12L10 7.737v8.526L16.394 12zm2.982.416L8.777 19.482A.5.5 0 0 1 8 19.066V4.934a.5.5 0 0 1 .777-.416l10.599 7.066a.5.5 0 0 1 0 .832z" />
         </svg>
       </button>
     </div>
